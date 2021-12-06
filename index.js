d95549b3f365e896332fce39292cb595f837daae
re-nu <renukaborade08@gmail.com>
2021-12-06 17:35:06 +0530

    crud on theater

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import { ObjectID } from 'bson';
 
 // https://hackaton2-node.herokuapp.com/  api url
 
@@ -22,10 +23,51 @@ async function createConnection() {
     return client;
 }
 
-const client=createConnection();
+const client= await createConnection();
 
 app.get("/",(request,response)=>{
     response.send("hello😊😊!!!");
+})
+
+app.post("/theaters",async(request,response)=>{
+    const data=request.body
+
+    const result=await client.db("b28wd").collection("theaters").insertMany(data);
+    response.send(result);
+})
+
+app.get("/theaters",async(request,response)=>{
+    const filter=request.query
+    const result= await client.db("b28wd").collection("theaters").find(filter).toArray();
+    response.send(result);
+})
+
+app.get("/theater/:name",async(request,response)=>{
+    const{name}=request.params
+    const result= await client.db("b28wd").collection("theaters").findOne({name:name});
+
+    result?response.send(result):response.status(404).send({message:"no theather of this name"})
+})
+
+app.put("/theater/:name",async(request,response)=>{
+    const{name}=request.params;
+    const data=request.body;
+    const result= await client.db("b28wd").collection("theaters").updateOne({name:name},{$set:data});
+    response.send(result);
+})
+
+app.post("/Add-theater",async(request,response)=>{
+    const data=request.body
+
+    const result=await client.db("b28wd").collection("theaters").insertOne(data);
+    response.send(result);
+})
+
+app.delete("/delete-theater/:id",async(request,response)=>{
+    const{id}=request.params;
+
+    const result=await client.db("b28wd").collection("theaters").deleteOne({_id:ObjectID(id)});
+    response.send(result);
 })
 
 app.listen(PORT,()=>{console.log("App started in PORT:",PORT)});
